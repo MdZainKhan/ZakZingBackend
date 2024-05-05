b2a222e7d2bc769d09d1fc9c250b0de69c3090c8
MdZainKhan <164855703+MdZainKhan@users.noreply.github.com>
2024-05-05 20:12:46 +0530

    index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     }
     res.json({
         success: 1, // If image successfully uploads it will respond with 1
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://zakzingbackend.onrender.com/images/${req.file.filename}`
     });
 });
 
